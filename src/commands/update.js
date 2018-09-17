8e4a476397330a371576cb8db8c83e3d1310be33
Avalander <darth.quesorico@gmail.com>
2018-09-17 15:08:09 +0200

    Yuki restarts when there is an update.

--- src/commands/update.js
+++ src/commands/update.js
@@ -10,6 +10,10 @@ module.exports = (text, message, { settings }) => {
         }
         const result = child_process.execSync('npm run bot:update').toString()
         console.log(result)
-        return message.channel.send(result)
+        if (result.includes('Already up-to-date.')) {
+            return message.channel.send(`This interface is already in sync with the latest version.`)
+        }
+        return message.channel.send(ACK)
+            .then(() => child_process.spawn('npm', [ 'run', 'bot:restart' ]))
     }
 }
