7ce014095eae5e18b6d6bebc4addb64105df763d
SavagePixie <atilaeldos@hotmail.com>
2019-11-12 19:53:10 +0100

    refactored code

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,24 +1,69 @@
-const { checkRole } = require('./util');
+const { evalTextStart, getClearance, makePipe } = require('./util')
 const rnd = require('../randtools')
 
+module.exports = makePipe(
+    evalTextStart((text, message, { memory }) => message.channel.send(checkPrecipitation(memory)), 'check precipitation', 'check rain'),
+    evalTextStart((text, message, { memory }) => message.channel.send(newPrecipitation(message, memory)), 'new precipitation', 'new rain'),
+    evalTextStart((text, message, { memory }) => message.channel.send(setPrecipitation(text, message, memory)), 'set precipitation', 'set rain'),
+    evalTextStart((text, message, { memory }) => message.channel.send(checkSeason(memory)), 'check season'),
+    evalTextStart((text, message, { memory }) => message.channel.send(newSeason(message, memory)), 'new season'),
+    evalTextStart((text, message, { memory }) => message.channel.send(setSeason(text, message, memory)), 'set season'),
+    evalTextStart((text, message, { memory }) => message.channel.send(checkTemperature(memory)), 'check temperature'),
+    evalTextStart((text, message, { memory }) => message.channel.send(newTemperature(message, memory)), 'new temperature'),
+    evalTextStart((text, message, { memory }) => message.channel.send(setTemperature(text, message, memory)), 'set temperature'),
+    evalTextStart((text, message, { memory }) => message.channel.send(checkWeather(memory)), 'check weather'),
+    evalTextStart((text, message, { memory }) => message.channel.send(newWeather(message, memory)), 'new weather'),
+    evalTextStart((text, message, { memory }) => message.channel.send(checkWind(memory)), 'check wind'),
+    evalTextStart((text, message, { memory }) => message.channel.send(newWind(message, memory)), 'new wind'),
+    evalTextStart((text, message, { memory }) => message.channel.send(setWind(text, message, memory)), 'set wind')
+)
+
+//Data
 const SEASONS = [{
     name: 'winter',
     precipitation: 30,
-    temperature: -1.25
+    temperature: -1.25,
 },{
     name: 'spring',
     precipitation: 70,
-    temperature: -0.25
+    temperature: -0.25,
 },{
     name: 'summer',
     precipitation: 15,
-    temperature: 0
+    temperature: 0,
 },{
     name: 'autumn',
     precipitation: 60,
-    temperature: -0.75
+    temperature: -0.75,
+}]
+
+const DESCRIPTIONS = {
+    precipitation: [
+        'No precipitation',
+        'Light fog (Visibility reduced to 3/4; -2 Perception checks and ranged attacks)',
+        'Medium fog (Visibility reduced by half; -4 Perception checks and ranged attacks)',
+        'Medium precipitation (Visibility reduced by half; -4 Perception checks and ranged attacks; automatically extinguishes unprotected flames (candles, torches, and the like))',
+        'Heavy precipitation (Visibility reduced to 1/4; -6 Perception checks and ranged attacks; automatically extinguishes unprotected flames)',
+    ],
+    temperature: [
+        'Below -30ºC (1d6 cold damage every minute (no save); Fort save (DC 15, +1 per previous check) or take 1d4 nonlethal damage)',
+        'Below -20ºC (Fort save every 10 min (DC 15, +1 per previous check) or take 1d6 points of nonlethal damage)',
+        'Below 5ºC (Fort save each hour (DC 15, +1 per previous check) or take 1d6 points of nonlethal damage)',
+        'Chilled',
+        'Cool',
+        'Warm',
+        'Above 30ºC (Fort save each hour (DC 15, +1 per previous check) or take 1d4 points of nonlethal damage)',
+        'Above 45ºC (Fort save every 10 min (DC 15, +1 per previous check) or take 1d4 points of nonlethal damage)',
+        'Above 60ºC (1d6 fire damage every minute (no save); Fort save every 5 minutes (DC 15, +1 per previous check) or take 1d4 nonlethal damage)',
+    ],
+    wind: [
+        '0-10 mph',
+        '11-20 mph',
+        '21-30 mph (-2 Fly and Perception checks, and ranged weapon attacks; check size Tiny)',
+        '31-50 mph (-4 Fly and Perception checks, and ranged weapon attacks; check size Small; blown away size Tiny)',
+        '51-74 mph (-8 Fly and Perception checks; ranged weapon attacks impossible; check size Medium; blown away size Tiny)',
+    ],
 }
-];
 
 const PRECIPTABLE = [{
     chance: 10,
@@ -34,42 +79,22 @@ const PRECIPTABLE = [{
     severity: 4,
 }]
 
-const PRECIPDESC =[
-    'No precipitation',
-    'Light fog (Visibility reduced to 3/4; -2 Perception checks and ranged attacks)',
-    'Medium fog (Visibility reduced by half; -4 Perception checks and ranged attacks)',
-    'Medium precipitation (Visibility reduced by half; -4 Perception checks and ranged attacks; automatically extinguishes unprotected flames (candles, torches, and the like))',
-    'Heavy precipitation (Visibility reduced to 1/4; -6 Perception checks and ranged attacks; automatically extinguishes unprotected flames)'
-];
-
 const TEMPTABLE = [{
     chance: 5,
-    variation: -1.5,
+    severity: -1.5,
 },{
     chance: 35,
-    variation: -0.5,
+    severity: -0.5,
 },{
     chance: 75,
-    variation: 0,
+    severity: 0,
 },{
     chance: 99,
-    variation: 0.5,
+    severity: 0.5,
 },{
-    chance: 99,
-    variation: 1.5,
-}];
-
-const TEMPDESC = [
-    'Below -30ºC (1d6 cold damage every minute (no save); Fort save (DC 15, +1 per previous check) or take 1d4 nonlethal damage)',
-    'Below -20ºC (Fort save every 10 min (DC 15, +1 per previous check) or take 1d6 points of nonlethal damage)',
-    'Below 5ºC (Fort save each hour (DC 15, +1 per previous check) or take 1d6 points of nonlethal damage)',
-    'Chilled',
-    'Cool',
-    'Warm',
-    'Above 30ºC (Fort save each hour (DC 15, +1 per previous check) or take 1d4 points of nonlethal damage)',
-    'Above 45ºC (Fort save every 10 min (DC 15, +1 per previous check) or take 1d4 points of nonlethal damage)',
-    'Above 60ºC (1d6 fire damage every minute (no save); Fort save every 5 minutes (DC 15, +1 per previous check) or take 1d4 nonlethal damage)'
-];
+    chance: 100,
+    severity: 1.5,
+}]
 
 const WINDTABLE = [{
     chance: 50,
@@ -86,204 +111,106 @@ const WINDTABLE = [{
 },{
     chance: 100,
     severity: 4,
-}];
+}]
 
-const WINDDESC = [
-    '0-10 mph',
-    '11-20 mph',
-    '21-30 mph (-2 Fly and Perception checks, and ranged weapon attacks; check size Tiny)',
-    '31-50 mph (-4 Fly and Perception checks, and ranged weapon attacks; check size Small; blown away size Tiny)',
-    '51-74 mph (-8 Fly and Perception checks; ranged weapon attacks impossible; check size Medium; blown away size Tiny)'
-];
-
-let currentSeason;
-let precipitation;
-let temperature;
-let wind;
+const regEx = {
+    firstLetter: /^\w/,
+    number: /-?[1-5]/,
+    seasons: /spring|winter|summer|autumn/,
+}
 
 //Utility functions.
-function findDesc (element) {
-    let table;
-    let desc;
-    switch (element) {
-        case 'precipitation':
-            table = PRECIPDESC;
-            desc = precipitation;
-            break;
-        case 'temperature':
-            table = TEMPDESC;
-            desc = temperature + 4;
-            break;
-        case 'wind':
-            table = WINDDESC;
-            desc = wind;
-            break;
-    }
-    return table[desc];
-}
+const doChecks = (message, memory, func) => getClearance(message, () => checkSeason(memory)
+    ? func()
+    : 'You need to set a season first.'
+)
 
 const rollChance = table => {
-    const elem = rnd.randInt(1, 100)
-    return table.find(x => elem <= x)
-}
-
-//Forecast functions.
-function checkForecast() {
-    let curP, curT, curW;
-    curP = checkPrecipitation();
-    curT = checkTemperature();
-    curW = checkWind();
-    return '*' + currentSeason.name + '*\n' + curP + '\n' + curT + '\n' + curW;
-}
-
-function newForecast(message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else if (!checkSeason()) return 'You need to set a season first.';
-    else{
-        newPrecipitation(message);
-        newTemperature(message);
-        newWind(message);
-        return checkForecast();
-    }
-}
-
-//Precipitation functions.
-function checkPrecipitation() {
-    return `**Precipitation**'s severity is ${precipitation + 1}: ${findDesc('precipitation')}.`;
-}
-
-function newPrecipitation(message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        if (!checkSeason()) return 'You need to set a season first.';
-        else{
-            let isRaining = rollD100();
-            if (isRaining < currentSeason.precipitation) precipitation = rollChance(PRECIPTABLE);
-            else precipitation = 0;
-            return checkPrecipitation();
-        }
-    }
-}
-
-//Temperature functions.
-function checkTemperature() {
-    let retT = Math.abs(temperature) + 1;
-    if (temperature == 0) retT += ' (Cold/Hot)';
-    if (temperature < 0) retT += ' (Cold)';
-    if (temperature > 0) retT += ' (Hot)';
-    return `**Temperature**'s severity is ${retT}: ${findDesc('temperature')}.`;
-}
-
-function newTemperature(message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        if (!checkSeason()) return 'You need to set a season first.';
-        else{
-            let tempVar = rollD100();
-            for (let i=0;i<TEMPTABLE.length;i++){
-                if (tempVar >= TEMPTABLE[i].minChance && tempVar <= TEMPTABLE[i].maxChance) {
-                    temperature = Math.round(currentSeason.temperature + TEMPTABLE[i].variation);
-                    break;
-                }
-            }
-            return checkTemperature();
-        }
-    }
-}
-
-//Wind functions.
-function checkWind() {
-    return `**Wind**'s severity is ${wind + 1}: ${findDesc('wind')}.`;
-}
-
-function newWind(message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        if (!checkSeason()) return 'You need to set a season first.';
-        else{
-            wind = rollChance(WINDTABLE);
-            return checkWind();
-        }
-    }
+    const chance = rnd.randInt(1, 100)
+    return table.find(x => chance <= x.chance)
 }
 
 //Weather functions.
-function setWeather(weather, text, message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        let severity = text.match(/-?\d+/);
-        let absSeverity = Math.abs(severity);
-        if (absSeverity >= 1 && absSeverity <= 5) {
-            switch (weather) {
-                case 'precipitation':
-                    precipitation = --severity;
-                    return checkPrecipitation();
-                case 'temperature':
-                    if (severity > 0) temperature = --severity;
-                    if (severity < 0) temperature = ++severity;
-                    return checkTemperature();
-                case 'wind':
-                    wind = --severity;
-                    return checkWind();
-            }
-        }else{
-            return 'Severity must be a number between 1 and 5 (or -1 and -5 for cold temperatures).';
-        }
-    }
+const checkWeather = memory => `${checkSeason(memory)}\n${checkPrecipitation(memory)}\n${checkTemperature(memory)}\n${checkWind(memory)}`
+
+const newWeather = (message, memory) => 
+    doChecks(message, memory, () => {
+        newPrecipitation(message, memory)
+        newTemperature(message, memory)
+        newWind(message, memory)
+        return checkWeather(memory)
+    })
+
+//Precipitation functions.
+const checkPrecipitation = memory => {
+    const rain = memory.get('precipitation')
+    return `**Precipitation**'s severity is ${rain + 1}: ${DESCRIPTIONS.precipitation[rain]}.`
 }
+
+const newPrecipitation = (message, memory) =>
+    setPrecipitation(rnd.randInt(1, 100) <= memory.get('season').precipitation
+        ? `${rollChance(PRECIPTABLE).severity + 1}`: '1', message, memory
+    )
+
+const setPrecipitation = (text, message, memory) => 
+    doChecks(message, memory, () => {
+        const match = text.match(regEx.number)
+        if (match == null || match[0] < 0) return 'Severity must be a number between 1 and 5.'
+        memory.set('precipitation', parseInt(match[0]) - 1)
+        return checkPrecipitation(memory)
+    })
+
+//Temperature functions.
+const checkTemperature = memory => {
+    const temp = memory.get('temperature')
+    return `**Temperature**'s severity is ${Math.abs(temp) + 1} (${temp > 0 ? "Hot" : temp == 0 ? "Cool" : "Cold"}): ${DESCRIPTIONS.temperature[temp + 4]}.`
+}
+
+const newTemperature = (message, memory) => {
+    const temp = Math.round(memory.get('season').temperature + rollChance(TEMPTABLE).severity)
+    return setTemperature(temp < 0 ? `${temp -1}` : `${temp + 1}`, message, memory)
+}
+
+const setTemperature = (text, message, memory) =>
+    doChecks(message, memory, () => {
+        const match = text.match(regEx.number)
+        if (match == null) return 'Severity must be a number between (-)1 and (-)5.'
+        const severity = parseInt(match[0])
+        memory.set('temperature', severity > 0 ? severity - 1 : severity + 1)
+        return checkTemperature(memory)
+    })
+
+//Wind functions.
+const checkWind = memory => {
+    const wind = memory.get('wind')
+    return `**Wind**'s severity is ${wind + 1}: ${DESCRIPTIONS.wind[wind]}.`
+}
+
+const newWind = (message, memory) => setWind(`${rollChance(WINDTABLE).severity + 1}`, message, memory)
+
+const setWind = (text, message, memory) =>
+    doChecks(message, memory, () => {
+        const match = text.match(regEx.number)
+        if (match == null || match[0] < 0) return 'Severity must be a number between 1 and 5.'
+        memory.set('wind', parseInt(match[0]) - 1)
+        return checkWind(memory)
+    })
 
 //Season functions.
-function checkSeason() {
-    if (currentSeason == undefined) return false;
-    else return currentSeason.name;
+const checkSeason = memory => {
+    const season = memory.get('season')
+    return Object.keys(season).length > 0
+        ? `We are in *${season.name.replace(regEx.firstLetter, x => x.toUpperCase())}*`
+        : false
 }
 
-function newSeason(message) {
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        let newSeason = Math.floor(Math.random() * SEASONS.length);
-        return setSeason(SEASONS[newSeason].name, message);
-    }
-}
+const newSeason = (message, memory) =>
+    getClearance(message, () => setSeason(rnd.randElem(SEASONS).name, message, memory))
 
-function setSeason(newSeason, message){
-    if (!checkRole(message.author.id, message.guild.roles)) return 'Sorry, you don\'t have permission to do that.';
-    else {
-        for (let i=0;i<SEASONS.length;i++){
-            if (SEASONS[i].name == newSeason){
-                currentSeason = SEASONS[i];
-                return 'The season has been set to ' + currentSeason.name + '. This is the current forecast.\n' + newForecast(message);
-            }
-        }
-    }
-}
-
-module.exports = (text, message) => {
-    text = text.toLowerCase();
-
-    if (text.startsWith('new forecast')) return message.channel.send(newForecast(message));
-    if (text.startsWith('check forecast')) return message.channel.send(checkForecast());
-
-    if (text.startsWith('set precipitation')) return message.channel.send(setWeather('precipitation', text, message));
-    if (text.startsWith('set temperature')) return message.channel.send(setWeather('temperature', text, message));
-    if (text.startsWith('set wind')) return message.channel.send(setWeather('wind', text, message));
-
-    if (text.startsWith('check precipitation')) return message.channel.send(checkPrecipitation());
-    if (text.startsWith('check temperature')) return message.channel.send(checkTemperature());
-    if (text.startsWith('check wind')) return message.channel.send(checkWind());
-
-    if (text.startsWith('new precipitation')) return message.channel.send(newPrecipitation(message));
-    if (text.startsWith('new temperature')) return message.channel.send(newTemperature(message));
-    if (text.startsWith('new wind')) return message.channel.send(newWind(message));
-
-    if (text.startsWith('set season')) {
-        let season = text.match(/winter|spring|summer|autumn/);
-        if (season == null) {
-            return message.channel.send('Sorry, I don\'t know that season.');
-        }else{
-            return message.channel.send(setSeason(season, message));
-        }
-    }
-    if (text.startsWith('check season')) return message.channel.send('The current season is ' + checkSeason() + '.');
-    if (text.startsWith('new season')) return message.channel.send(newSeason(message));
-}
+const setSeason = (text, message, memory) =>
+    getClearance(message, () => {
+        const newSeason = text.match(regEx.seasons)
+        if (newSeason == null) return 'Sorry, I have no data about that season.'
+        memory.set('season', SEASONS.find(x => x.name == newSeason[0]))
+        return `The season has been set to **${memory.get('season').name}**. This is the current weather.\n${newWeather(message, memory)}`
+    })
