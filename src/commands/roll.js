559872d8fea35771d4cd41d1d712c8bc6b660f91
SavagePixie <atilaeldos@hotmail.com>
2019-11-30 14:18:52 +0100

    improved way to extract roll expressions

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -11,30 +11,28 @@ module.exports =
     )
 
 const regEx = {
+    gGeneral: /(?:[+-])?\d+(?:d(?:\d+|f))?/g,
     gModifier: /[+-]\d+(?!d(?:\d+|f))/g,
     gRoll: /[+-](\d+)d(\d+|f)/g,
     roll: /[+-](\d+)d(\d+|f)/,
-    tEfs: /^f|[^d]f|\Ddf/g,
-    tNoRoll: /\Dd|d[^0-9f]|[a-ceg-z]/g,
     tSplitter: /(?=[^+-]\d+d(?:\d+|f))/,
-    tTrimmer: /[^-+0-9df\s]/g,
 }
 
 const roller = (dice, start, end) => Array(parseInt(dice)).fill(1).reduce((a) => {
     const roll = randInt(start, end)
-    return { result: a.result + roll, rolls: a.rolls.concat(roll) }
-}, { result: 0, rolls: [] })
+    return { result: a.result + roll, list: a.list.concat(roll) }
+}, { result: 0, list: [] })
   
 const reduceRolls = (a, b) => {
     const exp = b.match(regEx.roll)
 
     const isFudge = exp[2] == "f" ? true : false
     const roll = isFudge ? roller(exp[1], -1, 1) : roller(exp[1], 1, exp[2])
-    const rollArr = isFudge ? a.rolls.concat([roll.rolls.map(x => fudgify(x))]) : a.rolls.concat([roll.rolls])
+    const rollArr = isFudge ? a.list.concat([roll.list.map(x => fudgify(x))]) : a.list.concat([roll.list])
 
     return exp[0].startsWith("+") 
-        ? { result: a.result + roll.result, rolls: rollArr }
-        : { result: a.result - roll.result, rolls: rollArr }
+        ? { result: a.result + roll.result, list: rollArr }
+        : { result: a.result - roll.result, list: rollArr }
 }
 
 const reduceNumber = (a, b) => a + parseInt(b)
@@ -46,34 +44,44 @@ const addModifiers = (text, regEx, reducer, initVal) => {
 
 const addNumbers = text => addModifiers(text, regEx.gModifier, reduceNumber, 0)
 
-const addRolls = text => addModifiers(text, regEx.gRoll, reduceRolls, { result: 0, rolls: [] })
+const addRolls = text => addModifiers(text, regEx.gRoll, reduceRolls, { result: 0, list: [] })
 
 const parseRolls = (text, memory) => {
-    const str = getRollExps(text)
-    return str === ""
-        ? Object.entries(memory.get('defaultRoll')).length > 0
-            ? parseRolls(memory.get('defaultRoll'), memory)
-            : "Invalid expression"
-        : str.split(regEx.tSplitter)
-            .map(x => {
-                const rolls = addRolls(`+${x.trim()}`)
-                return `**${rolls.result + addNumbers(x)}** (${rolls.rolls.join("),(")}) [_${x.split(" ").join("")}_]`
-            })
-            .reduce((a, b) => `${a}\n${b}`, "There you go:")
+    const exps = getRollExps(text)
+    return exps.length >= 1
+        ? formatResult(exps)
+        : Object.entries(memory.get('defaultRoll')).length > 0
+        ? parseRolls(`roll ${memory.get('defaultRoll')}`, memory)
+        : 'Invalid expression'
 }
+
+const extractExpression = text => {
+    const expressions = []
+    text.replace(regEx.gGeneral, x => expressions.push(x))
+    return expressions.join('')
+}
+
+const formatExpression = expression => {
+    const rolls = addRolls(`+${expression}`)
+    return `**${rolls.result + addNumbers(expression)}** (${rolls.list.join('),(')}) [_${expression}_]`
+}
+
+const formatResult = expressions => expressions
+    .map(formatExpression)
+    .reduce((a, b) => `${a}\n${b}`, "There you go:")
 
 const fudgify = num => {
     const map = { "-1": "-", "0": " ", "1": "+" }
     return map[num]
 }
 
-const getRollExps = text => text.toLowerCase().replace(regEx.tEfs, "").replace(regEx.tNoRoll, "").replace(regEx.tTrimmer, "").trim()
-
+const getRollExps = text => text.toLowerCase().split(regEx.tSplitter).slice(1).map(x => extractExpression(x))
+    
 const setDefaultRoll = (text, message, memory) => checkClearance(message, () => {
-    const str = getRollExps(text).trim()
-    if (str === "") return "Invalid expression"
+    const expression = getRollExps(text).join(', ')
+    if (expression === "") return "Invalid expression"
     else {
-        memory.set('defaultRoll', str)
+        memory.set('defaultRoll', expression)
         return `I've set default roll to ${memory.get('defaultRoll')}.`
     }
 })
