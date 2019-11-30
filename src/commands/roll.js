ba2385ac15f4a6f32de0a904ad3ba0cb774ff2c1
SavagePixie <atilaeldos@hotmail.com>
2019-11-30 15:50:57 +0100

    fixed tSplitter splitting single digits

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -4,18 +4,24 @@ const { randInt } = require('../randtools')
 module.exports =
     makePipe(
         textContains('roll'),
-        (text, message, { memory }) => 
-            text.includes('set default roll')
+        (text, message, { memory }) =>
+            text.includes('--help')
+                ? message.channel.send(getHelp())
+                : text.includes('set default roll')
                 ? message.channel.send(setDefaultRoll(text, message, memory))
                 : message.channel.send(parseRolls(text, memory))
     )
+
+const FUDGE_MAP = { '-1': '-', '0': ' ', '1': '+' }
+
+const INVALID = 'Invalid expression. Type `roll --help` if you need to know more about this command.'
 
 const regEx = {
     gGeneral: /(?:[+-])?\d+(?:d(?:\d+|f))?/g,
     gModifier: /[+-]\d+(?!d(?:\d+|f))/g,
     gRoll: /[+-](\d+)d(\d+|f)/g,
     roll: /[+-](\d+)d(\d+|f)/,
-    tSplitter: /(?=[^+-]\d+d(?:\d+|f))/,
+    tSplitter: /(?=[^+-\d]\d+d(?:\d+|f))/,
 }
 
 const roller = (dice, start, end) => Array(parseInt(dice)).fill(1).reduce((a) => {
@@ -28,7 +34,7 @@ const reduceRolls = (a, b) => {
 
     const isFudge = exp[2] == 'f' ? true : false
     const roll = isFudge ? roller(exp[1], -1, 1) : roller(exp[1], 1, exp[2])
-    const rollArr = isFudge ? a.list.concat([roll.list.map(x => fudgify(x))]) : a.list.concat([roll.list])
+    const rollArr = isFudge ? a.list.concat([roll.list.map(x => FUDGE_MAP[x])]) : a.list.concat([roll.list])
 
     return exp[0].startsWith('+') 
         ? { result: a.result + roll.result, list: rollArr }
@@ -48,11 +54,12 @@ const executeRolls = text => addModifiers(text, regEx.gRoll, reduceRolls, { resu
 
 const parseRolls = (text, memory) => {
     const exps = getRollExps(text)
+    console.log('Expressions:', exps)
     return exps.length >= 1
         ? formatResult(exps)
         : Object.entries(memory.get('defaultRoll')).length > 0
         ? formatResult(memory.get('defaultRoll'))
-        : 'Invalid expression'
+        : INVALID
 }
 
 const extractExpression = text => {
@@ -70,25 +77,26 @@ const formatResult = expressions => expressions
     .map(formatExpression)
     .reduce((a, b) => `${a}\n${b}`, 'There you go:')
 
-const fudgify = num => {
-    const map = { "-1": "-", "0": " ", "1": "+" }
-    return map[num]
-}
-
 const getRollExps = text => text.toLowerCase().split(regEx.tSplitter).slice(1).map(x => extractExpression(x))
     
 const setDefaultRoll = (text, message, memory) => checkClearance(message, () => {
     const expression = getRollExps(text)
-    if (expression === '') return "Invalid expression"
+    if (expression.length < 1) return INVALID
     else {
         memory.set('defaultRoll', expression)
         return `I've set default roll to ${memory.get('defaultRoll').join(', ')}.`
     }
 })
 
-const getHelp = () => `This is the functionality of the module \`roll\`:\n${descriptions.map(x => `\`${x[0]}\`: ${x[1]}\n`)}`
+const getHelp = () => `I will roll dice for you.
+${descriptions.map(x => `- \`${x.command}\`: ${x.description}`).join('\n')}`
 
 const descriptions = [
-    [ "roll", "With this command, I will simulate a dice roll and return a result based on your expression.\nWrite [number]d[number] (v.gr. 2d6) and I will roll the appropriate die. You can also add as many modifiers (either static numbers or rolls) by appending a + or a - in front of them (v.gr. 2d6+3-1d4). You can also chain more than one roll (v.gr. 2d6+3, 2d6+5, 2d6-1d4)." ],
-    [ "set default roll", "**Only for GMs**. With this command, I will store your roll expression and use it when someone does not provide a valid roll expression with the command `roll`." ],
+    { 
+        command: 'roll',
+        description: 'I will simulate a dice roll and return a result based on your expression. You can chain more than one roll.\n\t`[number]d[number](+|-[number])`\n\tExamples: `2d6`, `2d8-1d4+3`, `1d10+3, 2d6-1d4+1d8+5 and 2d10-1d4`',
+    },{
+        command: 'set default roll',
+        description: '**Only for GMs**. I will store your roll expression and use it when someone does not provide a valid expression with `roll`.',
+    },
 ]
