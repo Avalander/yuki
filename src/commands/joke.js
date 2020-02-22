cf7bb5e461b31e3e7c2f21229b81151d8e8cc661
Avalander <darth.quesorico@gmail.com>
2020-02-22 11:01:00 +0100

    Add unit tests for joke command.

--- src/commands/joke.js
+++ src/commands/joke.js
@@ -2,50 +2,54 @@ const { get } = require('../request')
 
 const {
 	makePipe,
+	retry,
 	textMatches,
 } = require('./util')
 
 
-const fetchJoke = () => get({
-	host: 'icanhazdadjoke.com',
-	headers: {
-		Accept: 'application/json',
-	},
-})
+const handler = ({ get }) => {
+	const fetchJoke = () => get({
+		host: 'icanhazdadjoke.com',
+		headers: {
+			Accept: 'application/json',
+		},
+	})
 
-const retry = (fn, predicate, times = 10) =>
-	fn()
-		.then(data => predicate(data)
-			? data
-			: times > 0
-			? retry(fn, predicate, times - 1)
-			: Promise.reject({ success: false, data })
-		)
+	return (_, message, { memory }) => {
+		const active_period = Date.now() - (24 * 60 * 60 * 1000)
+		const latest_jokes = memory.get('joke:latest-jokes', [])
+			.filter(({ timestamp }) => timestamp > active_period)
 
-const handler = (_, message, { memory }) => {
-	const active_period = Date.now() - (24 * 60 * 60 * 1000)
-	const latest_jokes = memory.get('joke:latest-jokes', [])
-		.filter(({ timestamp }) => timestamp > active_period)
-
-	const is_new_joke = ({ id }) =>
-		latest_jokes.find(j => j.id == id) == null
-	
-	return retry(fetchJoke, is_new_joke, 3)
-		.catch(error => error.success === false
-			? Promise.resolve(error.data)
-			: Promise.reject(error)
-		)
-		.then(({ id, joke }) => Promise.all([
-			message.channel.send(joke),
-			memory.set('joke:latest-jokes', latest_jokes.concat({
-				id,
-				joke,
-				timestamp: Date.now(),
-			})),
-		]))
+		const is_new_joke = ({ id }) =>
+			latest_jokes.find(j => j.id == id) == null
+		
+		return retry(fetchJoke, is_new_joke, 3)
+			.catch(error => error.success === false
+				? Promise.resolve(error.data)
+				: Promise.reject(error)
+			)
+			.then(({ id, joke }) => Promise.all([
+				message.channel.send(joke),
+				memory.set('joke:latest-jokes', latest_jokes.concat({
+					id,
+					joke,
+					timestamp: Date.now(),
+				})),
+			]))
+			.catch(e => {
+				console.log('Error', e)
+				message.channel.send(`Something went wrong\n${e.statusCode} - ${e.message}`)
+			})
+	}
 }
 
 module.exports = makePipe(
 	textMatches(/(send me a )?joke( please)?(\.|!)?/i),
-	handler
+	handler({ get })
 )
+
+module.exports.factory = ({ get }) =>
+	makePipe(
+		textMatches(/(send me a )?joke( please)?(\.|!)?/i),
+		handler({ get })
+	)
