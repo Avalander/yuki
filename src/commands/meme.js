bc7f26535a0d628eea46578085709a0d5b30228e
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2020-02-24 19:49:35 +0100

    created meme module

---
+++ src/commands/meme.js
@@ -0,0 +1,27 @@
+const { get } = require('../request')
+const {
+	makePipe,
+	textContains,
+} = require('./util')
+
+const handler = ({ get }) => {
+	const fetchMeme = () => get({
+		host: 'some-random-api.ml',
+		path: '/meme',
+		headers: {
+			Accept: 'application/json',
+		},
+	})
+
+	return (_, message) => fetchMeme()
+		.then(meme => message.channel.send(meme.caption + '\n' + meme.image))
+		.catch(error => {
+			console.error(error)
+			message.channel.send('Something went wrong')
+		})
+}
+
+module.exports = makePipe(
+	textContains('meme'),
+	handler({ get })
+)
