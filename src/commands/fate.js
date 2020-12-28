9951752db68f11c229b9e5844af4aa389daac7aa
arturjzapater <zapater.artur@gmail.com>
2020-12-28 15:53:57 +0100

    updated commands to accept multiple characters

--- src/commands/fate.js
+++ src/commands/fate.js
@@ -3,11 +3,11 @@ const { makePipe, textMatches } = require('src/commands/util')
 const KEY = 'fate'
 
 const regExps = {
-    erase: /fate (\w+)?:?\s?erase/i,
+    erase: /fate (\w+(?:,\s?\w+)*)?:?\s?erase/i,
     list: /fate list/i,
-    gain: /fate (\w+) (?:g|gain):?\s?(\d+)?\D*$/i,
-    refresh: /fate (\w+) (?:r|refresh)/i,
-    setRefresh: /fate (\w+) (?:sr|set refresh):?\s?(\d+)\D*$/i,
+    gain: /fate (\w+(?:,\s?\w+)*) (?:g|gain):?\s?(\d+)?\D*$/i,
+    refresh: /fate (\w+(?:,\s?\w+)*) (?:r|refresh)/i,
+    setRefresh: /fate (\w+(?:,\s?\w+)*) (?:sr|set refresh):?\s?(\d+)\D*$/i,
     show: /fate (\w+) (?:sh|show)/i,
     spend: /fate (\w+(?:,\s?\w+)*) (?:sp|spend):?\s?(\d+)?\D*$/i,
 }
@@ -15,6 +15,10 @@ const regExps = {
 const makeCharacterList = str => str.split(/,\s?/)
 
 const makePointsMsg = character => `**${character.name}**: ${character.points}`
+
+const makeRefreshsMsg = character => `**${character.name}**: ${character.refresh}`
+
+const compareCharacters = (a, b) => a.name <= b.name ? -1 : 1
 
 const create = store => store.save(KEY, '[]')
     .then(() => '[]')
@@ -24,10 +28,12 @@ const loadOrCreate = store => store.load(KEY)
     .then(JSON.parse)
 
 const save = store =>
-    data =>
-        store
-            .save(KEY, JSON.stringify(data))
-            .then(() => data)
+    data => {
+        const sortedData = data.sort(compareCharacters)
+        return store
+            .save(KEY, JSON.stringify(sortedData))
+            .then(() => sortedData)
+    }
 
 const findCharacter = (name, data) =>
     data.find(x => x.name === name)
@@ -36,13 +42,18 @@ const findCharacter = (name, data) =>
 const findCharacterOrFail = name =>
     data => {
         const character = data.find(x => x.name === name)
-        if (!character) throw { code: 2, message: `${name} not found.`}
+        if (!character) throw `${name} not found.`
         return character
     }
 
-const findCharactersOrFail = names =>
-    data =>
-        names.map(x => findCharacterOrFail(x)(data))
+const findCharactersOrFail = input =>
+    data => {
+        const names = Array.isArray(input)
+            ? input
+            : makeCharacterList(input)
+
+        return names.map(x => findCharacterOrFail(x)(data))
+    }
 
 const makeCharacter = name => ({
     name,
@@ -55,29 +66,45 @@ const updateCharacter = (update, character) => ({
     ...update,
 })
 
-const refreshFate = name =>
+const updatePoints = amount =>
+    character =>
+        updateCharacter({ points: character.points + Number(amount) }, character)
+
+const updateRefresh = amount =>
+    character =>
+        updateCharacter({ refresh: Number(amount) }, character)
+
+const refreshPoints = character => {
+    const points = Math.max(character.points, character.refresh)
+    return updateCharacter({ points }, character)
+}
+
+const refreshFate = str =>
     data => {
-        const character = findCharacterOrFail(name)(data)
-        const points = Math.max(character.points, character.refresh)
+        const names = makeCharacterList(str)
+        const characters = findCharactersOrFail(names)(data)
         return data
-            .filter(x => x.name !== name)
-            .concat(updateCharacter({ points }, character))
+            .filter(x => !names.includes(x.name))
+            .concat(characters.map(refreshPoints))
     }
 
-const setRefresh = (refresh, name) =>
+const setRefresh = (refresh, str) =>
     data => {
-        const character = findCharacter(name, data)
+        const names = makeCharacterList(str)
+        const characters = names.map(x => findCharacter(x, data))
+
         return data
-            .filter(x => x.name !== name)
-            .concat(updateCharacter({ refresh: Number(refresh) }, character))
+            .filter(x => !names.includes(x.name))
+            .concat(characters.map(updateRefresh(refresh)))
     }
 
-const gainFate = (name, increase = 1) =>
+const gainFate = (str, increase = 1) =>
     data => {
-        const character = findCharacterOrFail(name)(data)
+        const names = makeCharacterList(str)
+        const characters = findCharactersOrFail(names)(data)
         return data
-            .filter(x => x.name !== name)
-            .concat(updateCharacter({ points: character.points + Number(increase) }, character))
+            .filter(x => !names.includes(x.name))
+            .concat(characters.map(updatePoints(increase)))
     }
 
 const spendFate = (str, decrease = 1) =>
@@ -94,49 +121,53 @@ const spendFate = (str, decrease = 1) =>
 
         return data
             .filter(x => !names.includes(x.name))
-            .concat(characters.map(x => updateCharacter({ points: x.points - decrease }, x)))
+            .concat(characters.map(updatePoints(-decrease)))
     }
 
 const eraseAll = store => store.remove(KEY)
     .then(() => 'All data')
 
-const eraseOne = (store, name) => loadOrCreate(store)
-    .then(data => data.filter(x => x.name != name))
-    .then(save(store))
-    .then(() => name)
+const eraseSome = (store, str) => {
+    const names = makeCharacterList(str)
+    return loadOrCreate(store)
+        .then(data => data.filter(x => !names.includes(x.name)))
+        .then(save(store))
+        .then(() => str)
+}
 
 module.exports.gainFate = makePipe(
     textMatches(regExps.gain),
     (text, message, { store }) => {
-        const [ , name, increase ] = text.match(regExps.gainFate)
+        const [ , names, increase ] = text.match(regExps.gain)
         return loadOrCreate(store)
-            .then(gainFate(name, increase))
+            .then(gainFate(names, increase))
             .then(save(store))
-            .then(findCharacterOrFail(name))
-            .then(data => message.channel.send(`${name}'s fate is now ${data.points}`))
+            .then(findCharactersOrFail(names))
+            .then(data => message.channel.send(`Fate gained. Current status: ${data.map(makePointsMsg).join(', ')}.`))
     }
 )
 
 module.exports.refreshFate = makePipe(
     textMatches(regExps.refresh),
     (text, message, { store }) => {
-        const [ , name ] = text.match(regExps.refresh)
+        const [ , names ] = text.match(regExps.refresh)
         return loadOrCreate(store)
-            .then(refreshFate(name))
+            .then(refreshFate(names))
             .then(save(store))
-            .then(findCharacterOrFail(name))
-            .then(data => message.channel.send(`${name} refreshed. Current fate: ${data.points}`))
+            .then(findCharactersOrFail(names))
+            .then(data => message.channel.send(`Fate refreshed. Current status: ${data.map(makePointsMsg).join(', ')}.`))
     }
 )
     
 module.exports.setRefreshFate = makePipe(
     textMatches(regExps.setRefresh),
     (text, message, { store }) => {
-        const [ , name, refresh ] = text.match(regExps.setRefresh)
+        const [ , names, refresh ] = text.match(regExps.setRefresh)
         return loadOrCreate(store)
-            .then(setRefresh(refresh, name))
+            .then(setRefresh(refresh, names))
             .then(save(store))
-            .then(() => message.channel.send(`${name}'s refresh set to ${refresh}.`))
+            .then(findCharactersOrFail(names))
+            .then(data => message.channel.send(`Fate refreshed. Current refresh: ${data.map(makeRefreshsMsg).join(', ')}.`))
     }
 )
 
@@ -157,7 +188,7 @@ module.exports.spendFate = makePipe(
         return loadOrCreate(store)
             .then(spendFate(names, decrease))
             .then(save(store))
-            .then(findCharactersOrFail(makeCharacterList(names)))
+            .then(findCharactersOrFail(names))
             .then(data => message.channel.send(`Fate spent. Current status: ${data.map(makePointsMsg).join(', ')}.`))
             .catch(error =>
                 error.code == 1
@@ -178,12 +209,12 @@ module.exports.listFate = makePipe(
 module.exports.eraseFate = makePipe(
     textMatches(regExps.erase),
     (text, message, { store }) => {
-        const [ , name ] = text.match(regExps.erase)
-        const fn = name
-            ? eraseOne
+        const [ , names ] = text.match(regExps.erase)
+        const fn = names
+            ? eraseSome
             : eraseAll
 
-        return fn(store, name)
+        return fn(store, names)
             .then(data => message.channel.send(`${data} removed.`))
     }
 )
